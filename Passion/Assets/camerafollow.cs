c3ba606c9d0619d782508a0b4dd4202de3907489
Zaviyae <36626311+Zaviyae@users.noreply.github.com>
2018-06-21 03:10:14 +0100

    all

---
+++ Passion/Assets/camerafollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerafollow : MonoBehaviour {
+
+    Transform player;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void LateUpdate()
+    {
+        this.transform.position = player.position;
+    }
+}
---
+++ Passion/Assets/clicktomove.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class clicktomove : MonoBehaviour {
+
+    private Animator nAnimator;
+
+    private NavMeshAgent nNavMeshAgent;
+
+    private bool isRunning = false;
+
+
+	// Use this for initialization
+	void Start () {
+
+        nAnimator = GetComponent<Animator>();
+        nNavMeshAgent = GetComponent<NavMeshAgent>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                nNavMeshAgent.destination = hit.point;
+            }
+        }
+
+        if (nNavMeshAgent.remainingDistance <= nNavMeshAgent.stoppingDistance)
+        {
+            isRunning = false;
+        }
+        else
+        {
+            isRunning = true;
+
+        }
+
+        nAnimator.SetBool("running", isRunning);
+	}
+}
